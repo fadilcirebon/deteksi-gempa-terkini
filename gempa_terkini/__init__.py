effb08b85223eaf04f26ace5ca77e21a37c8284b
fadil <fadil.rich@gmail.com>
2021-12-10 16:19:44 +0700

    Deteksi Gempa Terkini

--- gempa_terkini/__init__.py
+++ gempa_terkini/__init__.py
@@ -24,18 +24,37 @@ def ektraksi_data():
 
         result = soup.find('div', {'class', 'col-md-6 col-xs-6 gempabumi-detail no-padding'})
         result = result.findChildren('li')
-        for res in result:
-             print(res)
+        i=0
+        magnitudo = None
+        kedalaman = None
+        ls = None
+        bt = None
+        lokasi = None
 
-        magnitudo = 0
+        for res in result:
+             if i == 1:
+                 magnitudo= res.text
+             elif i == 2:
+                 kedalaman= res.text
+             elif i == 3:
+                 koordinat = res.text.split(' - ')
+                 ls = koordinat[0]
+                 bt = koordinat[1]
+             elif i == 4:
+                 lokasi= res.text
+             elif i == 5:
+                 tsunami = res.text
+
+             i = i + 1
 
         hasil = dict()
-        hasil['tanggal']=  tanggal #"08 Desember 2021"
-        hasil['waktu']= waktu #" 05:18:33 WIB"
-        hasil['magnitudo']= magnitudo #"3.4"
-        hasil['lokasi']= {'ls':6.71,'bt':107.35}
-        hasil['pusat gempa']= 'Pusat gempa berada di darat 18 km Barat Daya Purwakarta'
-        hasil['dirasakan']= ' Dirasakan (Skala MMI): III Cipeundeuy, III Cirata, III Maniis, II Purwakarta'
+        hasil['tanggal']=  tanggal
+        hasil['waktu']= waktu
+        hasil['magnitudo']= magnitudo
+        hasil['kedalaman']= kedalaman
+        hasil['koordinat']= {'ls':ls,'bt':bt}
+        hasil['lokasi']= lokasi
+        hasil['tsunami']= tsunami
         return hasil
     else:
         return None
@@ -47,6 +66,7 @@ def tampilkan_data(result):
     print(f"Tanggal {result['tanggal']}")
     print(f"Waktu{result['waktu']}")
     print(f"Magnitudo {result['magnitudo']}")
-    print(f"Lokasi: 'LS'= {result['lokasi']['ls']}, 'BT'={result['lokasi']['bt']}")
-    print(f"Pusat {result['pusat gempa']}")
-    print(f"Dirasakan {result['dirasakan']}")
+    print(f"Kedalaman {result['kedalaman']}")
+    print(f"Koordinat: 'LS'= {result['koordinat']['ls']}, 'BT'={result['koordinat']['bt']}")
+    print(f"Lokasi :  {result['lokasi']}")
+    print(f"Apakah Berpotensi Tsunami :  {result['tsunami']}")
