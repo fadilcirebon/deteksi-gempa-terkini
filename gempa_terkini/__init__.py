5ddbaf62db5cc7781968f4a488cd00dc88beef03
fadil <fadil.rich@gmail.com>
2021-12-10 15:14:58 +0700

    Deteksi Gempa Terkini

--- gempa_terkini/__init__.py
+++ gempa_terkini/__init__.py
@@ -16,14 +16,23 @@ def ektraksi_data():
     except Exception:
         return None
     if content.status_code ==200:
-        print (content.text)
-        #soup = BeautifulSoup(content)
-        #print(soup.prettify())
+        soup = BeautifulSoup(content.text, 'html.parser')
+        result = soup.find('span',{'class':'waktu'})
+        result = result.text.split(',')
+        tanggal = result[0]
+        waktu = result[1]
+
+        result = soup.find('div', {'class', 'col-md-6 col-xs-6 gempabumi-detail no-padding'})
+        result = result.findChildren('li')
+        for res in result:
+             print(res)
+
+        magnitudo = 0
 
         hasil = dict()
-        hasil['tanggal']= "08 Desember 2021"
-        hasil['waktu']= " 05:18:33 WIB"
-        hasil['magnitudo']= "3.4"
+        hasil['tanggal']=  tanggal #"08 Desember 2021"
+        hasil['waktu']= waktu #" 05:18:33 WIB"
+        hasil['magnitudo']= magnitudo #"3.4"
         hasil['lokasi']= {'ls':6.71,'bt':107.35}
         hasil['pusat gempa']= 'Pusat gempa berada di darat 18 km Barat Daya Purwakarta'
         hasil['dirasakan']= ' Dirasakan (Skala MMI): III Cipeundeuy, III Cirata, III Maniis, II Purwakarta'
@@ -35,9 +44,9 @@ def ektraksi_data():
 
 def tampilkan_data(result):
     print(f'Gempa Terakhir berdasarkan BMKG')
-    print(f"Tanggal{result['tanggal']}")
+    print(f"Tanggal {result['tanggal']}")
     print(f"Waktu{result['waktu']}")
-    print(f"Magnitudo{result['magnitudo']}")
-    print(f"Lokasi: 'LS'={result['lokasi']['ls']}, 'BT'={result['lokasi']['bt']}")
-    print(f"Pusat{result['pusat gempa']}")
-    print(f"Dirasakan{result['dirasakan']}")
+    print(f"Magnitudo {result['magnitudo']}")
+    print(f"Lokasi: 'LS'= {result['lokasi']['ls']}, 'BT'={result['lokasi']['bt']}")
+    print(f"Pusat {result['pusat gempa']}")
+    print(f"Dirasakan {result['dirasakan']}")
